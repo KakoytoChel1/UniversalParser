2fe58efe826f6485bccf3059d86b1cf3fb1c5c79
KakoytoChel777 <63608432+ivanchik-coder@users.noreply.github.com>
2022-08-12 11:35:28 +0300

    Some changes

---
+++ UniversalParser/Models/Parse.cs
@@ -0,0 +1,48 @@
+﻿using AngleSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp.Html.Dom;
+using AngleSharp.Dom;
+
+namespace UniversalParser.Models
+{
+    static internal class Parse
+    {
+        static public async Task<string> GetParseResultAsync(string url, string selector, bool hrefState)
+        {
+            string result = String.Empty;
+            try
+            {
+                var config = Configuration.Default.WithDefaultLoader();
+                var context = BrowsingContext.New(config);
+
+                var document = await context.OpenAsync(url);
+
+                IHtmlCollection<IElement> cells = document.QuerySelectorAll(selector);
+
+                var titles = cells.Select(m => m).ToList();
+
+                if (hrefState == true)
+                {
+                    foreach (var title in titles)
+                    {
+                        result += $"{((IHtmlAnchorElement)title).Href}\n";
+                    }
+                }
+                else
+                {
+                    foreach (var title in titles)
+                    {
+                        result += $"{((IHtmlElement)title).TextContent}\n";
+                    }
+                }
+            }
+            catch (Exception ex) { result = ex.Message; }
+
+            return result;
+        }
+    }
+}
---
+++ UniversalParser/ViewModels/MainViewModel.cs
@@ -0,0 +1,147 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using System.Windows;
+using UniversalParser.Models;
+using System.Windows.Controls;
+
+namespace UniversalParser.ViewModels
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+
+        public MainViewModel()
+        {
+            StateText = "Enter to start..";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        //tell user pasring is finished or not
+        private string _statetext;
+        public string StateText
+        {
+            get { return _statetext; }
+            set { _statetext = value; OnPropertyChanged(nameof(StateText));}
+        }
+
+        //true or false
+        private bool _statebool;
+        public bool Statebool
+        {
+            get { return _statebool; }
+            set { _statebool = value; OnPropertyChanged(nameof(Statebool));}
+        }
+
+        //show user result of parsing
+        private string _text;
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; OnPropertyChanged(nameof(Text)); }
+        }
+
+        //our target
+        private string _usrl;
+        public string Url
+        {
+            get { return _usrl; }
+            set { _usrl = value; OnPropertyChanged(nameof(Url)); }
+        }
+
+        private bool _hrefState;
+        public bool HrefState
+        {
+            get { return _hrefState; }
+            set { _hrefState = value; OnPropertyChanged(nameof(HrefState)); }
+        }
+
+        //our selector
+        private string _selectedSelector;
+        public string SelectedSelector
+        {
+            get { return _selectedSelector; }
+            set { _selectedSelector = value; OnPropertyChanged(nameof(SelectedSelector));
+                if(value == "a")
+                {
+                    HrefState = true;
+                }
+                else { HrefState = false; }
+            }
+        }
+        //close app
+        private ICommand _closeCommand;
+        public ICommand CloseAppCommand
+        {
+            get
+            {
+                if (_closeCommand == null)
+                {
+                    _closeCommand = new RelayCommand(p => {
+                        MainWindow win = p as MainWindow;
+                        win.Close();
+                    });
+                }
+                return _closeCommand;
+            }
+        }
+        //minimize app
+        private ICommand _minimizeCommand;
+        public ICommand MinimizeAppCommand
+        {
+            get
+            {
+                if (_minimizeCommand == null)
+                {
+                    _minimizeCommand = new RelayCommand(p => {
+                        MainWindow win = p as MainWindow;
+                        win.WindowState = WindowState.Minimized;
+                    });
+                }
+                return _minimizeCommand;
+            }
+        }
+
+        //start parsing
+        private ICommand _startpars;
+        public ICommand StartparsCommand
+        {
+            get
+            {
+                if (_startpars == null)
+                {
+                    _startpars = new RelayCommand(async p =>
+                    {
+                        Text = "Waiting for result...";
+                        StateText = "Parsing has been started...";
+                        Text = await Parse.GetParseResultAsync(Url, SelectedSelector, HrefState);
+                        StateText = "Parsing has been finished!";
+
+                    });
+                }
+                return _startpars;
+            }
+        }
+
+        private ICommand _managemenu;
+        public ICommand ManageMenuCommand
+        {
+            get
+            {
+                if (_managemenu == null)
+                {
+                    _managemenu = new RelayCommand(p =>
+                    {
+                        //if statebool is false then change text and in another case
+                    });
+                }
+                return _managemenu;
+            }
+        }
+    }
+}
